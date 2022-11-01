88fd689f0dca76dc2266c6c66b2cd0662d88dae9
Dimakamaz <117161440+Dimakamaz@users.noreply.github.com>
2022-11-01 16:06:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-import requests
-
-url = "https://genius-song-lyrics1.p.rapidapi.com/search"
-
-querystring = {"q":"Все идёт по плану","per_page":"10","page":"1"}
-
-headers = {
-	"X-RapidAPI-Key": "7868891ccamsh9707919760c6431p181ac5jsn0807b8ba7827",
-	"X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com"
-}
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
+import lyricsgenius 
+import telebot 
+ 
+token_bot = '5764000123:AAE9g6Xdrg_iPf2vESpSJ_Hlwp6sNtXxDlA' 
+bot = telebot.TeleBot(token_bot) 
+ 
+token = 'EnCFUWr6Gt190uRmIRezRf8ymCfMaQah1UZ9DwiunbvX0iXYqpt3YJFsV7FEG8oN' 
+genius = lyricsgenius.Genius(token) 
+ 
+@bot.message_handler(commands = ['start']) 
+def start(msg): 
+    bot.send_message(msg.chat.id, 'Введи название своей артиста/групы и трек через пробел.') 
+ 
+@bot.message_handler(content_types=['text']) 
+def lyrics (msg): 
+    result = msg.text.split('-') 
+ 
+    song = genius.search_song(result[1], result[0]) 
+    bot.send_message(msg.chat.id, song.lyrics) 
+ 
+ 
+bot.polling()
 
